eeb0df85441a8abdafd822aba3b0641619e2e03e
Anthony Finch <actionspeakslouder@gmail.com>
2014-08-24 15:18:55 +0100

    Constraints on want generation

--- desktop/src-common/ld30/core.clj
+++ desktop/src-common/ld30/core.clj
@@ -28,23 +28,27 @@
   (let [point (input->screen screen input-x input-y)]
     (get-entity-at-point entities point)))
 
-(defn set-location-wants
+(defn set-location-makes
   [entity]
   (assoc entity
         :makes (rand-nth products-list)))
 
-(defn set-location-makes
-  [entity]
+(defn set-location-wants
+  [entity entities]
   (let [wants (+ (rand-int 10) 1)
-        wants-item (rand-nth products-list)]
-    (assoc entity
-           :wants wants
-           :wants-item wants-item)))
+        wants-item (rand-nth (filter #(not= % (:makes entity)) (set (map #(:makes %) entities))))]
+    (if wants-item 
+      (assoc entity
+             :wants wants
+             :wants-item wants-item)
+      (assoc entity
+             :wants 0
+             :wants-item ""))))
 
 (defn update-location
-  [loc]
+  [loc entities]
   (let [loc(if (< (:wants loc) 1)
-             (set-location-makes loc)
+             (set-location-wants loc entities)
              loc)
         els(for [el (:entities loc)]
             (case (:id el)
@@ -58,7 +62,7 @@
   [x y]
   (let [w 200
         h 40]
-    (update-location (set-location-makes (set-location-wants (assoc (bundle (assoc (shape :filled
+    (set-location-makes (assoc (bundle (assoc (shape :filled
                                               :set-color (color :red)
                                               :rect 0 0 w h)
                                               :id :background)
@@ -77,12 +81,13 @@
                                :height h 
                                :location? true
                                :anger 0
-                               :center {:x (+ x (/ w 2)) :y (+ y (/ h 2))}))))))
+                               :wants-item ""
+                               :wants 0
+                               :center {:x (+ x (/ w 2)) :y (+ y (/ h 2))}))))
 
 
 (defn make-link
   [e1 e2]
-  (println e1 e1)
   (let [x1 (:x (:center e1))
         y1 (:y (:center e1))
         x2 (:x (:center e2))
@@ -122,10 +127,10 @@
     (let [connected (connected-to-want entity entities)]
       (if connected
         (update! screen :score (+ (:score screen) 1)))
-      (let [entity(if connected
+      (let [entity(if (and connected (:wants entity))
                     (assoc entity :wants (- (:wants entity) 1))
                     (assoc entity :anger (+ (:anger entity) 1)))]
-        (update-location entity)
+        (update-location entity entities)
       ))
     (= (:id entity) :score)
     (doto entity (label! :set-text (format "Score: %d" (:score screen))))
@@ -142,13 +147,11 @@
 
 (defn make-active-link
   [screen entities loc]
-  (println "placing first point")
   (update! screen :active-link-point (:center loc))
   entities)
 
 (defn add-link
   [screen entities loc active-link-point]
-  (println "placing second point")
   (update! screen :active-link-point false)
   (conj entities (make-link loc (get-entity-at-point entities active-link-point)))) 
 
@@ -159,19 +162,15 @@
              :score 0
              :renderer (stage))
     (add-timer! screen :pulse 1 1)
-    (let [loc1 (make-location 40 35)
-          loc2 (make-location 400 150)
-          loc3 (make-location 250 500)
-          loc4 (make-location 60 400)]
-      [loc1
-       loc2
-       loc3
-       loc4
-       (make-link loc1 loc3)
-       (make-link loc1 loc2)
-       (assoc (label "Score: 0" (color :white))
+    (let [locs[(make-location 40 35)
+               (make-location 400 150)
+               (make-location 250 500)
+               (make-location 60 400)]
+          locs(for [l locs]
+                (update-location l locs))]
+      (conj locs (assoc (label "Score: 0" (color :white))
             :id :score
-            :x 15 :y 570)]))
+            :x 15 :y 570))))
 
   :on-key-down
   (fn [screen entities]
@@ -179,7 +178,7 @@
       (= (:key screen) (key-code :a))
       (for [entity entities]
         (if (:location? entity)
-        (update-location entity) entity))
+        (update-location entity entities) entity))
       (= (:key screen) (key-code :q))
       (app! :exit)
       ))
